4ce2f588db3a497dc3ff324ecfe5dc92c1d4c9c0
EarvinKayonga <demoneartin@gmail.com>
2015-12-04 18:13:28 +0100

    Semi header

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,13 @@
-angular.module('BnpApp',[])
-.controller('mainController' ,function($scope){
-  console.log("YOLO");
-});
+angular.module('BnpApp', [])
+  .controller('mainController', function($scope, $rootScope) {
+    $rootScope.signOut = function(){
+      $rootScope.user = {};
+    };
+  }).controller('headController', function($scope, $rootScope) {
+    $scope.auth = function(arg){
+      arg["firstname"] = 'Dylan';
+      arg["lastname"] = 'Gillard';
+      $rootScope.user = arg;
+    };
+
+  });
